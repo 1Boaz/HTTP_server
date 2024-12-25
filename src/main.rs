41fed24cd6cb68b046a5b54cf3e626f09f75ece7
1Boaz <bizbizdrori@gmail.com>
2024-12-25 21:24:19 +0200

    added read the option to read from file with args

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,23 @@
 use actix_web::{get, App, HttpResponse, HttpServer, Responder};
+use actix_web::middleware::Logger;
+use std::fs;
+
+const ARGS: Vec<String> = std::env::args().collect::<Vec<String>>();
 
 #[get("/")]
 async fn return_html() -> impl Responder {
-    HttpResponse::Ok().body(r#"
-    <!doctype html>
-    <html>
-    <head>
-    <meta charset="utf-8">
-    </head>
-    <body>
-    <a href="https://www.rust-lang.org">rust-lang</a>"#)
+    let body = fs::read_to_string(&ARGS[0]);
+    match body {
+        Ok(body) => HttpResponse::Ok().content_type("text/html").body(body),
+        Err(_) => HttpResponse::Ok().content_type("text/html").body("was not able to read file"),
+    }
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
         App::new()
+            .wrap(Logger::default())
             .service(return_html)
     })
         .bind(("127.0.0.1", 8080))?
