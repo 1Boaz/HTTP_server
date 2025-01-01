1733ebdd9c0196da62430087856f8b1c305fc902
1Boaz <bizbizdrori@gmail.com>
2025-01-01 20:57:09 +0200

    First try of serving static files(without reading the HTML to String)

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,13 @@
-use actix_web::{get, App, HttpResponse, HttpServer, Responder};
+use actix_files::NamedFile;
 use actix_web::middleware::Logger;
-use std::fs;
+use actix_web::HttpRequest;
+use actix_web::{web, App, HttpServer};
 use std::env;
+use std::path::PathBuf;
 
-// Func to read HTML file and send it to the browser
-#[get("/")]
-async fn return_html() -> impl Responder {
-    let mut file_path = "index.html";
-    // Read the file path from command-line arguments.
-    let args: Vec<String> = env::args().collect();
-    // checks if was an input
-    if args.len() > 2 {
-        file_path = &args[1];
-    }
-    // logs the HTML
-    println!("read file: {}", &file_path);
-    // returns the HTML
-    match fs::read_to_string(file_path) {
-        Ok(body) => HttpResponse::Ok().content_type("text/html").body(body),
-        Err(_) => HttpResponse::InternalServerError()
-            .content_type("text/html")
-            .body("Could not read the specified file."),
-    }
+async fn index(req: HttpRequest) -> actix_web::Result<NamedFile> {
+    let path: PathBuf = req.match_info().query("filename").parse()?;
+    Ok(NamedFile::open(path)?)
 }
 
 #[actix_web::main]
@@ -29,9 +15,11 @@ async fn main() -> std::io::Result<()> {
     // creates the default host and port
     let mut address = "127.0.0.1";
     let mut port: u16 = 8080;
+    let mut file_path = "index.html";
     // takes args and puts them in place(in case they were inserted)
     let args: Vec<String> = env::args().collect();
     if args.len() > 3 {
+        file_path = &args[1];
         if args[2] != "" {
             address = args[2].as_str();
         }
@@ -39,13 +27,13 @@ async fn main() -> std::io::Result<()> {
             port = (&args[3]).parse().unwrap();
         }
     }
-    // starts the server and creates the return_html route
+
     HttpServer::new(|| {
         App::new()
+            .route(file_path, web::get().to(index))
             .wrap(Logger::default())
-            .service(return_html)
     })
-        .bind((address, port))?
-        .run()
-        .await
+    .bind((address, port))?
+    .run()
+    .await
 }
